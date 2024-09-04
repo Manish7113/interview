11b669558febcbc644344047490eb1a6ba0ca6a7
Manish7113 <kumawatji7113@gmail.com>
2024-09-04 23:36:30 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  const [rowCol, setRowCol] = useState({
+    row: 0,
+    col: 0,
+  })
+
+  const [arr, setArr] = useState([])
+
+  const handleClick = () => {
+    const myarr = new Array(rowCol?.row).fill(' ').map(()=> new Array(rowCol.col).fill(' '));
+    setArr(myarr)
+    console.log(myarr , '----------------------------')
+   
+   
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='d-flex justify-content-center align-items-center flex-column w-100 gap-5 pt-5 mb-5 pb-5'>
+      <h1 >Dynamic State</h1>
+      <div className='w-100 d-flex justify-content-center align-items-center gap-5 '>
+      <input className='w-25 p-2 m-1 '  type='number' placeholder='enter no of rows' onChange={(e) => {
+        setRowCol({ ...rowCol, row: Number(e.target.value) })
+      }}></input>
+      <input className='w-25 p-2 m-1 ' type='number' placeholder='enter no of col' onChange={(e) => {
+        setRowCol({ ...rowCol, col: Number(e.target.value) })
+      }}></input>
+
+      </div>
+      <button className='btn btn-primary ' onClick={handleClick}>Submit</button>
+      
+      {arr.map((f, i) => {
+        return (
+          <div className='w-75  p-2 d-flex justify-content-center align-items-center flex-wrap gap-2' style={{border:'1px solid blue'}}>
+            {arr[i].map((f, j) => {
+              return (
+                <input type='text' className='w-15  inputText' placeholder={i + " " + j} onChange={(e)=>{
+                  setArr((prevArr) => {
+                    const updatedArr = [...prevArr];
+                    updatedArr[i][j] = e.target.value; // Replace 'newValue' with the desired value
+                    return updatedArr;
+                  });
+
+                }}></input>
+              )
+            })}
+
+
+          </div>
+        )
+      })}
+      <table className='w-75 d-flex justiyf-content-center align-items-center flex-column mt-5'>
+      {arr.map((e, i) => {
+        return (
+          <tr className='w-100 d-flex justiyf-content-center align-items-center mt-1' style={{border:'1px solid black'}}>
+            {arr[i].map((e, j) => {
+              return (
+                <td className='w-50 d-flex justiyf-content-center align-items-center p-2 ' style={{border:'1px solid black'}}> {arr[i][j]}</td>
+              )
+            })}
+
+
+          </tr>
+        )
+      })}
+
+      </table>
     </div>
+
   );
 }
 
